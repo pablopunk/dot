7dcf2d3343dd0ae6cc1bb38b52886c56b778a691
Pablo P Varela <pablo@pablopunk.com>
2025-07-30 15:56:13 +0200

    use colors in script

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -2,9 +2,17 @@
 
 set -e # exit on error
 
+function green() {
+  echo -e "\033[32m$1\033[0m"
+}
+
+function red() {
+  echo -e "\033[31m$1\033[0m"
+}
+
 # check $1 if it's a valid version number
 if ! [[ "$1" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-  echo "Invalid version number"
+  red "Invalid version number"
   echo "Usage: $0 <version>"
   echo "Example: $0 2.0.0"
   exit 1
@@ -12,13 +20,13 @@ fi
 
 # Check if we're on main
 if [ "$(git rev-parse --abbrev-ref HEAD)" != "main" ]; then
-  echo "Not on main branch"
+  red "Not on main branch"
   exit 1
 fi
 
 # Check if git status is clean
 if git status --porcelain | grep -q '^[MADRCU]'; then
-  echo "Git status is not clean"
+  red "Git status is not clean"
   exit 1
 fi
 
@@ -29,7 +37,7 @@ sed -i '' "s/version = \".*\"/version = \"$APP_VERSION\"/g" dot.lua
 
 version=$(./dot.lua --version)
 if [ "$version" != "dot version $APP_VERSION" ]; then
-  echo "Version in dot.lua does not match $APP_VERSION"
+  red "Version in dot.lua does not match $APP_VERSION"
   exit 1
 fi
 
@@ -43,8 +51,8 @@ git push --tags
 
 set +x
 
-echo
-echo "Successfully updated version to $APP_VERSION"
-echo "The new release should be automatically created on GitHub:"
-echo "https://github.com/pablopunk/dot/releases"
-echo
+green
+green "Successfully updated version to $APP_VERSION"
+green "The new release should be automatically created on GitHub:"
+green "https://github.com/pablopunk/dot/releases"
+green
