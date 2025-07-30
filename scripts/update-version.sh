2d8a5e7c8039300c53efa4665e401da8a4e8f490
Pablo P Varela <pablo@pablopunk.com>
2025-07-30 15:39:08 +0200

    add scripts

---
+++ scripts/update-version.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e # exit on error
+
+## APP_VERSION=0.9.11 gck -b "$APP_VERSION" && gcam "$APP_VERSION" && g tag "$APP_VERSION" && gp && gp --tags
+
+# check $1 if it's a valid version number
+if ! [[ "$1" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+  echo "Invalid version number"
+  echo "Usage: $0 <version>"
+  echo "Example: $0 0.9.11"
+  exit 1
+fi
+
+APP_VERSION="$1"
+
+git checkout -b "$APP_VERSION"
+
+git commit -m "bump version to $APP_VERSION"
+
+git tag "$APP_VERSION"
+
+git push origin "$APP_VERSION"
+
+git push origin --tags
+
+echo "Successfully updated version to $APP_VERSION"
+echo "The new release should be automatically created on GitHub:"
+echo "https://github.com/pablopunk/dot/releases"
